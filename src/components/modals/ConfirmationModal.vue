3af5ba9e9eb041082b08dc111fc620942f579b4b
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-07-29 16:47:45 +0300

    feat(ConfirmationModal): add modal component with title and message props

---
+++ src/components/modals/ConfirmationModal.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+})
+
+const emit = defineEmits(['confirm', 'cancel'])
+</script>
+
+<template>
+    <div class="modal-backdrop">
+
+        <div class="modal">
+            <h2>{{ title }}</h2>
+            <p>{{ message }}</p>
+
+            <div class="modal-actions">
+                <button class="btn btn-secondary" @click="emit('cancel')">Cancel</button>                
+                <button class="btn btn-primary" @click="emit('confirm')">Confirm</button>
+            </div>
+        </div>
+
+    </div>
+</template>
