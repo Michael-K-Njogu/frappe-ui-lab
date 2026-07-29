d0a74efe9cf8fae640f3349196a55f4c836b5374
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-07-29 21:35:30 +0300

    feat(SearchInput): add search input component with model binding and placeholder

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    modelValue: {
+        type: String,
+        default: ''
+    },
+    placeholder: {
+        type: String,
+        default: 'Search...'
+    }
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <input
+        type="search"
+        class="search-input"
+        :value="modelValue"
+        :placeholder="placeholder"
+        @input="emit('update:modelValue', $event.target.value)"
+    />
+</template>
