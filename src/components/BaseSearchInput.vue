3b7f5ca7babfeaad4561ea07859aef779a5400a1
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-07-29 21:37:25 +0300

    feat(BaseSearchInput): create search input component with model binding and placeholder

---
+++ src/components/BaseSearchInput.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Search } from '@lucide/vue'
+
+defineProps({
+    modelValue: {
+        type: String,
+        default: ''
+    },
+    placeholder: {
+        type: String,
+        default: 'Search...'
+    }
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <div class="search-input-container">
+        <Search size="20" class="search-icon" />
+    <input
+        type="search"
+        class="search-input"
+        name="search"
+        :value="modelValue"
+        :placeholder="placeholder"
+        @input="emit('update:modelValue', $event.target.value)"
+    />
+    </div>
+</template>
