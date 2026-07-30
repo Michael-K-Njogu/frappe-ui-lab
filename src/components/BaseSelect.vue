6dc313ea9a963b921aced26d39c658a2fb394366
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-07-30 13:37:49 +0300

    feat(BaseSelect): add BaseSelect component with model binding and options rendering

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+    modelValue: {
+        type: String,
+        default: ''
+    },
+    options: {
+        type: Array,
+        default: () => []
+    },
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <select
+        class="base-select"
+        name="base-select"
+        :value="modelValue"
+        @change="emit('update:modelValue', $event.target.value)"
+    >
+        <option value="">All Types</option>
+        <option
+            v-for="option in options"
+            :key="option.value"
+            :value="option.value"
+        >
+            {{ option.label }}
+        </option>
+    </select>
+</template>
