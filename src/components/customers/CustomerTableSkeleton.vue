702bbafb643f561b0ed90b117f0798a6cd1c7ff3
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-01 00:38:33 +0300

    feat(BaseSkeleton): create BaseSkeleton component for reusable skeleton loading UI feat(CustomerTableSkeleton): implement CustomerTableSkeleton using BaseSkeleton for consistent loading state

---
+++ src/components/customers/CustomerTableSkeleton.vue
@@ -0,0 +1,29 @@
+<script setup>
+import BaseSkeleton from '../base/BaseSkeleton.vue'
+</script>
+
+<template>
+    <div class="customer-table-skeleton">
+
+        <div class="table-header">
+            <BaseSkeleton width="100%" height="20px" />
+            <BaseSkeleton width="100%" height="20px" />
+            <BaseSkeleton width="100%" height="20px" />
+            <BaseSkeleton width="100%" height="20px" />
+            <BaseSkeleton width="100%" height="20px" />
+        </div>
+
+        <div 
+            v-for="row in 5"
+            :key="row"
+            class="table-row"
+        >
+            <BaseSkeleton width="100%" height="20px"/>
+            <BaseSkeleton width="100%" height="20px"/>
+            <BaseSkeleton width="100%" height="20px"/>
+            <BaseSkeleton width="100%" height="20px"/>
+            <BaseSkeleton width="100%" height="20px"/>
+        </div>
+
+    </div>
+</template>
