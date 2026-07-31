737044aec0929c115662740ec3d30e3bad482539
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-01 02:05:15 +0300

    feat(toast): add Toast and ToastContainer components for notification display

--- src/components/toast/Toast.vue
+++ src/components/toast/Toast.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useToast } from '../composables/useToast'
+import { useToast } from '../../composables/useToast'
 import { computed } from 'vue'
 import {
     CircleCheck,
--- src/components/toast/ToastContainer.vue
+++ src/components/toast/ToastContainer.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useToast } from '../composables/useToast'
+import { useToast } from '../../composables/useToast.js'
 import Toast from './Toast.vue'
 import { TransitionGroup } from 'vue'
 
