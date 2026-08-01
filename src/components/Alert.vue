852d49be1b295a6e8e6c74457ca0e2f5c45a6af9
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-02 00:50:59 +0300

    feat(Alert): add new alert component with customizable message, title, and type

---
+++ src/components/Alert.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+  type: {
+    type: String,
+    default: 'info',
+  },
+})
+
+</script>
+
+<template>
+    <div class="alert" :class="`alert-${type}`">
+
+        <div class="alert-icon">
+            <slot name="icon">
+                <component v-if="icon" :is="icon" size="18" />
+            </slot>
+        </div>
+
+        <div class="alert-content">
+            <p>{{ message }}</p>
+        </div>
+
+    </div>
+</template>
