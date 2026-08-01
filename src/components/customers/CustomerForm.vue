9bd3344813834365bb6f8e4849804aa7545e4c5a
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-01 20:44:31 +0300

    fix(CustomerForm): improve form validation and styling for inputs

--- src/components/customers/CustomerForm.vue
+++ src/components/customers/CustomerForm.vue
@@ -4,6 +4,7 @@ import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import { customerTypes } from '../../config/customerTypes'
 import FormLabel from '../FormLabel.vue'
+import BaseSelect from '../BaseSelect.vue'
 
 const props = defineProps({
     initialValues: {
@@ -63,37 +64,48 @@ const fields = {
 </script>
 
 <template>
-   <form @submit.prevent="onSubmit">
-        <div class="form-group">
-            <FormLabel label="Name" for="name" required />
-            <input v-model="fields.name.value" name="name" id="name" autocomplete="name" placeholder="Enter name" />
-            <p v-if="errors.name" class="invalid">{{ errors.name }}</p>
+   <form class="form-container" @submit.prevent="onSubmit">
+        <div class="form-row">
+
+            <div class="form-group">
+                <FormLabel label="Name" for="name" required />
+                <input :class="{ 'is-invalid': errors.name }" v-model="fields.name.value" name="name" id="name" autocomplete="name" placeholder="Enter name" />
+                <p v-if="errors.name" class="invalid">{{ errors.name }}</p>
+            </div>
+
+            <div class="form-group">
+                <FormLabel label="Email" for="email" required />
+                <input :class="{ 'is-invalid': errors.email }" v-model="fields.email.value" name="email" id="email" autocomplete="email" placeholder="Enter email" />
+                <p v-if="errors.email" class="invalid">{{ errors.email }}</p>
+            </div>    
+
         </div>
 
-        <div class="form-group">
-            <FormLabel label="Email" for="email" required />
-            <input v-model="fields.email.value" name="email" id="email" autocomplete="email" placeholder="Enter email" />
-            <p v-if="errors.email" class="invalid">{{ errors.email }}</p>
-        </div>
+        <div class="form-row">
 
-        <div class="form-group">
-            <FormLabel label="Credit Limit" for="creditLimit" required />
-            <input v-model.number="fields.creditLimit.value" name="creditLimit" id="creditLimit" type="number" placeholder="Enter credit limit" />
-            <p v-if="errors.creditLimit" class="invalid">{{ errors.creditLimit }}</p>
-        </div>
+            <div class="form-group">
+                <FormLabel label="Credit Limit" for="creditLimit" required />
+                <input :class="{ 'is-invalid': errors.creditLimit }" v-model.number="fields.creditLimit.value" name="creditLimit" id="creditLimit" type="number" placeholder="Enter credit limit" />
+                <p v-if="errors.creditLimit" class="invalid">{{ errors.creditLimit }}</p>
+            </div>
 
-        <div class="form-group">
-            <FormLabel label="Customer Type" for="customerType" required />
-            <select v-model="fields.customerType.value" name="customerType" id="customerType">
-                <option value="">Select type</option>
-                <option v-for="type in customerTypes" :key="type.value" :value="type.value">{{ type.label }}</option>
-            </select>
-            <p v-if="errors.customerType" class="invalid">{{ errors.customerType }}</p>
+            <div class="form-group">
+                <FormLabel label="Customer Type" for="customerType" required />
+                <BaseSelect
+                    :class="{ 'is-invalid': errors.customerType }"
+                    v-model="fields.customerType.value"
+                    :options="customerTypes"
+                    all-options-selected-text="All Customer Types"
+                />
+                <p v-if="errors.customerType" class="invalid">{{ errors.customerType }}</p>
+            </div>  
+
         </div>
 
         <div class="form-group" v-if="props.showTerms">
-            <FormLabel label="Accept Terms and Conditions" for="acceptedTerms" required />
-            <input type="checkbox" v-model="fields.acceptedTerms.value" name="acceptedTerms" id="acceptedTerms" />
+            
+            <input :class="{ 'is-invalid': errors.acceptedTerms }" type="checkbox" v-model="fields.acceptedTerms.value" name="acceptedTerms" id="acceptedTerms" />
+            <FormLabel label="Accept Terms and Conditions" for="acceptedTerms" required inline />
             <p v-if="errors.acceptedTerms" class="invalid">{{ errors.acceptedTerms }}</p>
         </div>
 
