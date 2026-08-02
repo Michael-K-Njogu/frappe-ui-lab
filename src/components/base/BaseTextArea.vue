0f6b1bb30f9d7e0321d03c8b13fc0f0466f4e0db
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-03 00:29:11 +0300

    feat(BaseTextArea): add new component for text area input with model binding

---
+++ src/components/base/BaseTextArea.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+    modelValue: {
+        type: String,
+        default: ''
+    },
+    placeholder: {
+        type: String,
+        default: ''
+    },
+    type: {
+        type: String,
+        default: 'text'
+    }
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <textarea
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="emit('update:modelValue', $event.target.value)"
+    ></textarea>
+</template>
