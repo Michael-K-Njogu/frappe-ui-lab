42ac2a1955cb948e8140492c109600a04831c4f2
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-04 13:35:30 +0300

    feat(customerPermissions): add functions to manage customer deletion and editing permissions

---
+++ src/business/customerPermissions.js
@@ -0,0 +1,9 @@
+/* Customer Permissions */
+
+export function canDeleteCustomer(customer) {
+    return customer.orderCount === 0;
+}
+
+export function canEditCustomer(customer) {
+    return true; // All customers can be edited
+}
