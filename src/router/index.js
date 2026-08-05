3789ebc5ea70033e699bbdf04bf5e3abde1d802f
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-05 22:06:35 +0300

    feat(Router): reorganize imports and add product routes to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import AppLayout from '../layouts/AppLayout.vue'
+
+// Dashboard
 import DashboardView from '../views/DashboardView.vue'
+
+// Customers
 import CustomersView from '../views/CustomersView.vue'
-import OrdersView from '../views/OrdersView.vue'
 import CustomerDetailView from '../views/CustomerDetailView.vue'
 import NewCustomerView from '../views/NewCustomerView.vue'
 import EditCustomerView from '../views/EditCustomerView.vue'
+
+// Orders
+import OrdersView from '../views/OrdersView.vue'
+import OrderDetailView from '../views/OrderDetailView.vue'
+import NewOrderView from '../views/NewOrderView.vue'
+import EditOrderView from '../views/EditOrderView.vue'
+
+// Products
+import ProductsView from '../views/ProductsView.vue'
+import ProductDetailView from '../views/ProductDetailView.vue'
+import NewProductView from '../views/NewProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -60,7 +74,22 @@ const router = createRouter({
           path: 'orders/:id/edit',
           name: 'order-edit',
           component: () => import('../views/EditOrderView.vue'),
-        }
+        },
+        {
+          path: 'products',
+          name: 'products',
+          component: () => import('../views/ProductsView.vue'),
+        },
+        {
+          path: 'products/:id',
+          name: 'product-details',
+          component: () => import('../views/ProductDetailView.vue'),
+        },
+        {
+          path: 'products/new',
+          name: 'product-new',
+          component: () => import('../views/NewProductView.vue'),
+        },
       ],
     },
   ],
