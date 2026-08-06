a593e825874fd6ba026ad9ad199c1efbd6c9eef7
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-06 13:08:29 +0300

    feat(ProductTableSkeleton): add skeleton component for product table loading state

---
+++ src/components/products/ProductTableSkeleton.vue
@@ -0,0 +1,40 @@
+<script setup>
+import BaseSkeleton from '../base/BaseSkeleton.vue'
+
+const { rows } = defineProps({
+    rows: {
+        type: Number,
+        default: 5
+    }
+})
+
+const columns = 7 // Assuming there are 7 columns in the product table
+</script>
+
+<template>
+    <div class="data-table-skeleton">
+
+        <div class="data-table-skeleton__header">
+            <BaseSkeleton 
+                v-for="col in columns"
+                :key="col"
+                width="100%" 
+                height="20px" 
+            />
+        </div>
+
+        <div 
+            v-for="row in rows"
+            :key="row"
+            class="data-table-skeleton__row"
+        >
+            <BaseSkeleton 
+                v-for="col in columns"
+                :key="col"
+                width="100%" 
+                height="20px"
+            />
+        </div>
+
+    </div>
+</template>
