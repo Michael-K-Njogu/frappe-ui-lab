5a8ae43b2548afadeb12e22ae54f591c1c6e0714
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-06 13:44:47 +0300

    feat(BaseFormLabel): add BaseFormLabel component for form labeling

---
+++ src/components/base/BaseFormLabel.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  for: {
+    type: String,
+    required: true,
+  },
+  required: {
+    type: Boolean,
+    default: true,
+  },
+  inline: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <label :for="for" :class="['form-label', { 'form-label__inline': inline }]">
+    {{ label }}
+    <span v-if="required" class="required">*</span>
+  </label>
+</template>
