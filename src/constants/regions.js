b4f8b50c2038d8d1e645dad6b152f03e982e1a52
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-18 21:32:15 +0300

    feat(regions.js): add list of regions in Kenya

---
+++ src/constants/regions.js
@@ -0,0 +1,13 @@
+// List of regions in Kenya
+export const REGIONS = [
+  { value: 'Nairobi', label: 'Nairobi' },
+  { value: 'Mombasa', label: 'Mombasa' },
+  { value: 'Kisumu', label: 'Kisumu' },
+  { value: 'Nakuru', label: 'Nakuru' },
+  { value: 'Eldoret', label: 'Eldoret' },
+  { value: 'Thika', label: 'Thika' },
+  { value: 'Kitale', label: 'Kitale' },
+  { value: 'Naivasha', label: 'Naivasha' },
+  { value: 'Machakos', label: 'Machakos' },
+  { value: 'Meru', label: 'Meru' },
+]
