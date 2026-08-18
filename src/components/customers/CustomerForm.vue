acbb253655fa8f1ee6993da1fe5c136c7582c839
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-18 21:30:19 +0300

    feat(CustomerForm.vue): enhance customer form with additional fields and structured sections

--- src/components/customers/CustomerForm.vue
+++ src/components/customers/CustomerForm.vue
@@ -1,13 +1,23 @@
 <script setup>
 import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
+
 import { customerTypes } from '../../constants/customerTypes.js'
+import { SECTORS } from '../../constants/industryTypes.js'
+import { REGIONS } from '../../constants/regions.js'
 
 import BaseFormLabel from '../base/BaseFormLabel.vue'
 import BaseSelect from '../base/BaseSelect.vue'
+import BaseSearchableSelect from '../base/BaseSearchableSelect.vue'
 import BaseTextInput from '../base/BaseTextInput.vue'
+import BaseImageUpload from '../base/BaseImageUpload.vue'
 import BaseButton from '../base/BaseButton.vue'
 import BaseFormContainer from '../base/BaseFormContainer.vue'
+import BaseFormSection from '../base/BaseFormSection.vue'
+
+import Alert from '../Alert.vue'
+
+import { User, Mail, Truck, CreditCard, Handshake, Info } from '@lucide/vue'
 
 const props = defineProps({
 
@@ -15,9 +25,23 @@ const props = defineProps({
         type: Object,
         default: () => ({
             name: '',
-            email: '',
-            creditLimit: null,
             customerType: '',
+            sector: '',
+            region: '',
+            vatNumber: '',
+
+            contactPerson: '',
+            phonePrimary: '',
+            phoneSecondary: '',
+            email: '',
+
+            deliveryAddress: '',
+            city: '',
+
+            image: null,
+
+            creditLimit: null,
+            creditStatus: '',
             acceptedTerms: false,
         })
     },
@@ -61,82 +85,303 @@ const onSubmit = handleSubmit((values) => {
 
 const fields = {
     name: defineField('name')[0],
-    email: defineField('email')[0],
-    creditLimit: defineField('creditLimit')[0],
     customerType: defineField('customerType')[0],
+    sector: defineField('sector')[0],
+    region: defineField('region')[0],
+    vatNumber: defineField('vatNumber')[0],
+
+    contactPerson: defineField('contactPerson')[0],
+    email: defineField('email')[0],
+    phonePrimary: defineField('phonePrimary')[0],
+    phoneSecondary: defineField('phoneSecondary')[0],
+
+    deliveryAddress: defineField('deliveryAddress')[0],
+    city: defineField('city')[0],
+
+    image: defineField('image')[0],
+
+    creditLimit: defineField('creditLimit')[0],
+    creditStatus: defineField('creditStatus')[0],
+
+    notes: defineField('notes')[0],
     acceptedTerms: defineField('acceptedTerms')[0],
 }
 
 </script>
 
 <template>
-   <BaseFormContainer>
+
        <form @submit.prevent="onSubmit">
-            <div class="form-row">
 
-                <div class="form-group">
-                    <BaseTextInput
-                        name="name"
-                        id="name"
-                        label="Customer Name"
-                        :class="{ 'is-invalid': errors.name }"
-                        v-model="fields.name.value"
-                        placeholder="Enter customer name"
-                    />
-                    <p v-if="errors.name" class="invalid">{{ errors.name }}</p>
-                </div>
+            <!-- General Customer Details -->
+            <BaseFormSection title="Customer Information">
 
-                <div class="form-group">
-                    <BaseTextInput  
-                        :class="{ 'is-invalid': errors.email }"
-                        v-model="fields.email.value"
-                        label="Customer Email"
-                        name="email"
-                        id="email"
-                        placeholder="Enter customer email"
-                        type="email"
-                    />
-                    <p v-if="errors.email" class="invalid">{{ errors.email }}</p>
-                </div>    
+                <template #icon>
+                    <User />
+                </template>
 
-            </div>
+                <template #content>
+                    
+                    <div class="form-group">
+                        <BaseImageUpload
+                            v-model="fields.image.value"
+                            label="Customer / Business Logo"
+                            hint="Upload a JPG, JPEG or PNG, max 2MB."
+                        />   
+                        <p v-if="errors.image" class="invalid">{{ errors.image }}</p>
+                    </div>     
 
-            <div class="form-row">
+                    <div class="form-grid">
 
-                <div class="form-group">
-                    <BaseTextInput
-                        :class="{ 'is-invalid': errors.creditLimit }"
-                        v-model.number="fields.creditLimit.value"
-                        label="Credit Limit"
-                        name="creditLimit"
-                        id="creditLimit"
-                        placeholder="Enter credit limit"
-                        type="number"
-                    />
-                    <p v-if="errors.creditLimit" class="invalid">{{ errors.creditLimit }}</p>
-                </div>
+                        <div class="form-group">
+                            <BaseTextInput
+                                name="name"
+                                id="name"
+                                label="Customer / Business Name"
+                                :class="{ 'is-invalid': errors.name }"
+                                v-model="fields.name.value"
+                                placeholder="Enter customer name"
+                            />
+                            <p v-if="errors.name" class="invalid">{{ errors.name }}</p>
+                        </div>
 
-                <div class="form-group">
-                    <BaseSelect
-                        name="customerType"
-                        id="customerType"
-                        label="Customer Type"
-                        :class="{ 'is-invalid': errors.customerType }"
-                        v-model="fields.customerType.value"
-                        :options="customerTypes"
-                        all-options-selected-text="-- Select a customer type --"
-                    />
-                    <p v-if="errors.customerType" class="invalid">{{ errors.customerType }}</p>
-                </div>  
+                        <div class="form-group">
+                            <BaseSelect
+                                name="customerType"
+                                id="customerType"
+                                label="Customer Type"
+                                :class="{ 'is-invalid': errors.customerType }"
+                                v-model="fields.customerType.value"
+                                :options="customerTypes"
+                                all-options-selected-text="-- Select a customer type --"
+                            />
+                            <p v-if="errors.customerType" class="invalid">{{ errors.customerType }}</p>
+                        </div>                          
 
-            </div>
+                        <div class="form-group">
+                            <BaseSearchableSelect
+                                placeholder="Search and select a sector"
+                                name="sector"
+                                id="sector"
+                                label="Sector / Industry"
+                                :error="errors.sector"
+                                :class="{ 'is-invalid': errors.sector }"
+                                v-model="fields.sector.value"
+                                :options="SECTORS"
+                                all-options-selected-text="Select a sector"
+                            />
+                            <p v-if="errors.sector" class="invalid">{{ errors.sector }}</p>
+                        </div>   
 
-            <div class="form-group" v-if="props.showTerms">
-                
-                <input :class="{ 'is-invalid': errors.acceptedTerms }" type="checkbox" v-model="fields.acceptedTerms.value" name="acceptedTerms" id="acceptedTerms" />
-                <BaseFormLabel label="Accept Terms and Conditions" for="acceptedTerms" required inline />
-                <p v-if="errors.acceptedTerms" class="invalid">{{ errors.acceptedTerms }}</p>
-            </div>
+                        <div class="form-group">
+                            <BaseSearchableSelect
+                                placeholder="Search and select a region"
+                                name="region"
+                                id="region"
+                                label="Region"
+                                :error="errors.region"
+                                :class="{ 'is-invalid': errors.region }"
+                                v-model="fields.region.value"
+                                :options="REGIONS"
+                                all-options-selected-text="Select a region"
+                            />
+                            <p v-if="errors.region" class="invalid">{{ errors.region }}</p>
+                        </div>
+
+                        <div class="form-group">
+                            <BaseTextInput
+                                name="vatNumber"
+                                id="vatNumber"
+                                label="VAT Number"
+                                :class="{ 'is-invalid': errors.vatNumber }"
+                                v-model="fields.vatNumber.value"
+                                placeholder="Enter VAT number"
+                            />
+                            <p v-if="errors.vatNumber" class="invalid">{{ errors.vatNumber }}</p>
+                        </div>                        
+
+                    </div>                    
+                </template>
+            
+            </BaseFormSection>
+
+            <BaseFormSection title="Primary Contact">
+
+                <template #icon>
+                    <Mail />
+                </template>
+
+                <template #content>
+                    <div class="form-grid">
+
+                        <div class="form-group">
+                            <BaseTextInput
+                                name="contactPerson"
+                                id="contactPerson"
+                                label="Contact Person"
+                                v-model="fields.contactPerson.value"
+                                :class="{ 'is-invalid': errors.contactPerson }"
+                                placeholder="Enter contact person's name"
+                            />
+                            <p v-if="errors.contactPerson" class="invalid">
+                                {{ errors.contactPerson }}
+                            </p>
+                        </div>
+
+                        <div class="form-group">
+                            <BaseTextInput
+                                name="phonePrimary"
+                                id="phonePrimary"
+                                label="Phone Number"
+                                v-model="fields.phonePrimary.value"
+                                :class="{ 'is-invalid': errors.phonePrimary }"
+                                placeholder="Enter phone number"
+                                type="tel"
+                            />
+                            <p v-if="errors.phonePrimary" class="invalid">
+                                {{ errors.phonePrimary }}
+                            </p>
+                        </div>
+
+                        <div class="form-group">
+                            <BaseTextInput
+                                :required="false"
+                                name="phoneSecondary"
+                                id="phoneSecondary"
+                                label="Secondary Phone Number"
+                                v-model="fields.phoneSecondary.value"
+                                :class="{ 'is-invalid': errors.phoneSecondary }"
+                                placeholder="Enter secondary phone number"
+                                type="tel"
+                            />
+                            <p v-if="errors.phoneSecondary" class="invalid">
+                                {{ errors.phoneSecondary }}
+                            </p>
+                        </div>
+
+                        <div class="form-group">
+                            <BaseTextInput
+                                name="email"
+                                id="email"
+                                label="Email Address"
+                                v-model="fields.email.value"
+                                :class="{ 'is-invalid': errors.email }"
+                                placeholder="Enter email address"
+                                type="email"
+                            />
+                            <p v-if="errors.email" class="invalid">
+                                {{ errors.email }}
+                            </p>
+                        </div>
+
+                    </div>
+                </template>
+            </BaseFormSection>
+
+            <BaseFormSection title="Delivery Information">
+
+                <template #icon>
+                    <Truck />
+                </template>
+
+                <template #content>
+                    <div class="form-grid">
+
+                        <div class="form-group form-group-full">
+                            <BaseTextInput
+                                name="deliveryAddress"
+                                id="deliveryAddress"
+                                label="Delivery Address"
+                                v-model="fields.deliveryAddress.value"
+                                :class="{ 'is-invalid': errors.deliveryAddress }"
+                                placeholder="Enter delivery address"
+                            />
+                            <p v-if="errors.deliveryAddress" class="invalid">
+                                {{ errors.deliveryAddress }}
+                            </p>
+                        </div>
+
+                        <div class="form-group">
+                            <BaseTextInput
+                                name="city"
+                                id="city"
+                                label="City / Town"
+                                v-model="fields.city.value"
+                                :class="{ 'is-invalid': errors.city }"
+                                placeholder="Enter city or town"
+                            />
+                            <p v-if="errors.city" class="invalid">
+                                {{ errors.city }}
+                            </p>
+                        </div>
+
+                    </div>
+                </template>
+            </BaseFormSection>    
+            
+            <BaseFormSection title="Credit & Payment">
+
+                <template #icon>
+                    <CreditCard />
+                </template>
+
+                <template #content>
+                    <div class="form-grid">
+
+                        <div class="form-group">
+                            <BaseTextInput
+                                name="creditLimit"
+                                id="creditLimit"
+                                label="Credit Limit"
+                                v-model.number="fields.creditLimit.value"
+                                :class="{ 'is-invalid': errors.creditLimit }"
+                                placeholder="Enter credit limit"
+                                type="number"
+                                min="0"
+                            />
+                            <p v-if="errors.creditLimit" class="invalid">
+                                {{ errors.creditLimit }}
+                            </p>
+                        </div>
+
+                        <div class="form-group">
+                            <BaseSelect
+                                name="creditStatus"
+                                id="creditStatus"
+                                label="Credit Status"
+                                :class="{ 'is-invalid': errors.creditStatus }"
+                                v-model="fields.creditStatus.value"
+                                :options="[
+                                    { label: 'Active', value: 'Active' },
+                                    { label: 'Suspended', value: 'Suspended' },
+                                ]"
+                                all-options-selected-text="-- Select credit status --"
+                            />                          
+                            <p v-if="errors.creditStatus" class="invalid">
+                                {{ errors.creditStatus }}
+                            </p>
+                        </div>
+
+                    </div>
+                </template>
+            </BaseFormSection>            
+            
+            <BaseFormSection title="Terms & Conditions" v-if="props.showTerms">
+
+                <template #icon>
+                    <Info />
+                </template>                
+
+                <template #content>
+                    <div class="form-group">
+                        <p>By accepting, you agree to the terms and conditions.</p>
+                        <input :class="{ 'is-invalid': errors.acceptedTerms }" type="checkbox" v-model="fields.acceptedTerms.value" name="acceptedTerms" id="acceptedTerms" />
+                        <BaseFormLabel label="Accept Terms and Conditions" for="acceptedTerms" required inline />
+                        <p v-if="errors.acceptedTerms" class="invalid">{{ errors.acceptedTerms }}</p>
+                    </div>  
+                </template>              
+
+            </BaseFormSection>
 
             <div class="form-actions">
 
@@ -158,5 +403,4 @@ const fields = {
             </div>
 
         </form> 
-    </BaseFormContainer>
 </template>
