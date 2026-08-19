6123bb9db73a1b21c80e929d5130a5e26bd4f0e9
Michael-K-Njogu <mykeknjogu@gmail.com>
2026-08-20 00:24:36 +0300

    feat(AppLayout.vue): integrate NotificationMenu component into the top bar header

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,12 +1,28 @@
 <script setup>
+import { useRouter } from 'vue-router'
+
 import AppSidebarNav from '../components/AppSidebarNav.vue'
+import NotificationMenu from '../components/notifications/NotificationMenu.vue'
+
+const router = useRouter()
 </script>
 
 <template>
   <div class="layout">
     <AppSidebarNav />
     <main>
-      <header class="top-bar-header">DDS Enterprise Application</header>
+      <header class="top-bar-header">
+        <div class="top-bar-header-left"></div>
+        <div class="top-bar-header-right">
+          <NotificationMenu
+            @view-all="
+              router.push({
+                name: 'notifications',
+              })
+            "
+          />
+        </div>
+      </header>
       <div class="content">
         <RouterView />
       </div>
